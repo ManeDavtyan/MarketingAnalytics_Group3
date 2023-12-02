925bb30cfeb8e4f7cdfdfc67070a91f99fbfd2e1
manedavtyan <mane_davtyan@edu.aua.am>
2023-12-02 18:15:46 +0400

    adjustments

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    author='Karen Hovhannisyan',
-    description='DS-223 ETL staff',
+    author='Mane Davtyan',
+    description='BookStore Recommendation System',
     name='bookstore',
     version='0.1.0',
     packages=find_packages(include=['bookstore','bookstore.*']),
