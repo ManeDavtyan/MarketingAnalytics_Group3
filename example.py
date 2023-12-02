a36c66a6ce2924914c8e5477e94c99d90ec8714a
manedavtyan <mane_davtyan@edu.aua.am>
2023-12-02 18:11:51 +0400

    adjustments

---
+++ example.py
@@ -0,0 +1,31 @@
+
+# db
+# from bookstore import db
+# import pandas as pd
+#
+# authors = pd.read_csv(".//data//authors.csv")
+# books = pd.read_csv(".//data//books.csv")
+# customers = pd.read_csv(".//data//customers.csv")
+# inventory= pd.read_csv(".//data//inventory.csv")
+# orderitem = pd.read_csv(".//data//orderitem.csv")
+# orders = pd.read_csv(".//data//orders.csv")
+# publishers = pd.read_csv(".//data//publishers.csv")
+#
+# db.schema.create_database(authors, books, customers, inventory, orderitem, orders, publishers)
+#
+
+
+# api
+# from bookstore.api.api import app
+#
+# if __name__ == "__main__":
+#     import uvicorn
+#
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
+#
+
+
+
+# model
+
+# ...
