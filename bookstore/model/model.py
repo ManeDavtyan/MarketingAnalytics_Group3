6d71daf7edfb890a3f7cd7b70f8ed6ecd14f836b
GorMkrtchyan7 <gormkrtchyan02@gmail.com>
2023-12-06 22:37:04 +0400

    Modified model.py

--- bookstore/model/model.py
+++ bookstore/model/model.py
@@ -1,66 +1,45 @@
+
+import sqlite3
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.decomposition import TruncatedSVD
-import sqlite3
 
 # Connect to the SQLite database
 db_path = './/BookStore.db'
 conn = sqlite3.connect(db_path)
 
-# Assuming you have a table named 'books' in the database
 books_query = "SELECT * FROM books"
 books = pd.read_sql_query(books_query, conn)
 conn.close()
 
 # Preprocess the 'books' DataFrame
-books['content'] = books['title'] + ' ' + books['author_id'].astype(str) + ' ' + books['genre']
+books['content'] = books['title'] + ' ' + books['author_id'].astype(str) + ' ' + books['Genre']
 
 # TF-IDF Vectorization
 tfidf_vectorizer = TfidfVectorizer(stop_words='english')
 tfidf_matrix = tfidf_vectorizer.fit_transform(books['content'])
 
 # Include the 'rating' as a numerical feature
-rating_matrix = books['rating'].values.reshape(-1, 1)
+rating_matrix = books['Rating'].values.reshape(-1, 1)
 
 combined_matrix = pd.concat([pd.DataFrame(tfidf_matrix.toarray()), pd.DataFrame(rating_matrix)], axis=1)
 
-svd = TruncatedSVD(n_components=100)  # Adjust the number of components as needed
+svd = TruncatedSVD(n_components=100)
 tfidf_matrix_reduced = svd.fit_transform(tfidf_matrix)
 
 # Compute cosine similarity
 cosine_sim = linear_kernel(tfidf_matrix_reduced, tfidf_matrix_reduced)
 
 def get_recommendations(title, books_data):
-    matching_books = books_data.loc[books_data['title'] == title]
-
-    if matching_books.empty:
-        print(f"No books found with title: {title}")
-        return pd.DataFrame()  # Return an empty DataFrame
-
-    idx = matching_books.index[0]
+    idx = books_data.loc[books_data['title'] == title].index[0]
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-
-    # Check if there are similar books to recommend
-    if len(sim_scores) < 2:
-        print(f"No similar books found for: {title}")
-        return pd.DataFrame()  # Return an empty DataFrame
-
     sim_scores = sim_scores[1:6]  # Get top 5 similar books
     book_indices = [i[0] for i in sim_scores]
-    recommended_books = books_data.iloc[book_indices].copy()
-
-    # Create a DataFrame to store the results
-    result_df = pd.DataFrame({'input_book_title': [title] * len(recommended_books),
-                              'recommended_book_title': recommended_books['title'].tolist(),
-                              'author_id': recommended_books['author_id'].tolist(),
-                              'genre': recommended_books['genre'].tolist(),
-                              'price': recommended_books['price'].tolist(),
-                              'rating': recommended_books['rating'].tolist(),
-                              'similarity_score': [i[1] for i in sim_scores]})
-
-    return result_df
+    recommended_books = books_data.iloc[book_indices].copy()  # Create a copy to avoid modifying the original DataFrame
+    return recommended_books
 
 # Get user input for the book title
 title_to_recommend = input("Enter a book title: ")
